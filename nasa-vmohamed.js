6ae24520aac4d3acb26a78adce55ffff0c643058
BOUBTANE <mohamed-ali2000@hotmail.fr>
2022-01-05 17:37:51 +0100

    finallyjs1

--- nasa-vmohamed.js
+++ nasa-vmohamed.js
@@ -1,29 +1,32 @@
-const getData = async() => {
-    const body = Document
-    const dateStart = document.getElementById("start")
-    const dateFin = document.getElementById("end")
-    const key = "h1l0oYA0cbNuoknqZgeM6JrGnXe2MYnuBa0OFPCG"
+button.addEventListener("click", async() => {
 
-    const url = `https:api.nasa.gov/neo/rest/v1/feed?start_date=${dateStart}&end_date=${dateFin}&api_key=${key}`
+            const body = document.getElementsByTagName("body")[0]
+            const dateStart = document.getElementById("start")
+            const dateFin = document.getElementById("end")
+            const key = "h1l0oYA0cbNuoknqZgeM6JrGnXe2MYnuBa0OFPCG"
 
-    const response = await fetch(url);
-    const data = await response.json();
+            const url = `https:api.nasa.gov/neo/rest/v1/feed?start_date=${dateStart}&end_date=${dateFin}&api_key=${key}`
 
-    console.log(data);
+            const response = await fetch(url);
+            const data = await response.json();
 
-    const mapsElem = document.createElement("a");
-    mapsElem.className = "mapsButton";
-    mapsElem.innerText = "Google Maps"
-    mapsElem.href = infos.maps.googleMaps;
+            console.log(data);
+
+            const Object = document.createElement("a");
+            mapsElem.className = "object";
 
 
-    resultats.innerHTML = `
-  <h3>${pays}</h3>
-  <p><strong>Capital:</strong> ${infos.capital[0]}</p>
-  <p><strong>Continent:</strong> ${infos.continents[0]}</p>
-  <p><strong>Superficie:</strong> ${infos.area}</p>
-  <p><strong>Vents:</strong> ${meteo.wind.speed} km/h</p>
+
+
+            resultats.innerHTML = `
+  
+  <p><strong>Capital:</strong> ${}</p>
+  <p><strong>Continent:</strong> ${}</p>
+  <p><strong>Superficie:</strong> ${}</p>
+  <p><strong>Vents:</strong> ${} km/h</p>
 `
-    console.log(meteo.weather)
-    resultats.appendChild(mapsElem)
-}
+
+
+        }
+
+        // "element_count":25,"near_earth_objects":{"2015-09-08":[{"links":{"self":"http://www.neowsapp.com/rest/v1/neo/2465633?api_key=DEMO_KEY"},"id":"2465633","neo_reference_id":"2465633","name":"465633 (2009 JR5)","nasa_jpl_url":"http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2465633","absolute_magnitude_h":20.36,"estimated_diameter":{"kilometers":{"estimated_diameter_min":0.2251930467,"estimated_diameter_max":0.5035469604},"meters":{"estimated_diameter_min":225.1930466786,"estimated_diameter_max":503.5469604336},"miles":{"estimated_diameter_min":0.1399284286,"estimated_diameter_max":0.3128894784},"feet":{"estimated_diameter_min":738.8223552649,"estimated_diameter_max":1652.0570096689}},"is_potentially_hazardous_asteroid":true,"close_approach_data":[{"close_approach_date":"2015-09-08","close_approach_date_full":"2015-Sep-08 20:28","epoch_date_close_approach":1441744080000,"relative_velocity":{"kilometers_per_second":"18.1279547773","kilometers_per_hour":"65260.6371983344","miles_per_hour":"40550.4220413761"},"miss_distance":{"astronomical":"0.3027478814","lunar":"117.7689258646","kilometers":"45290438.204452618","miles":"28142173.3303294084"},"orbiting_body":"Earth"}],"is_sentry_object":false},{"links":{"self":"http://www.neowsapp.com/rest/v1/neo/3426410?api_key=DEMO_KEY"},"id":"3426410","neo_reference_id":"3426410","name":"(2008 QV11)","nasa_jpl_url":"http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3426410","absolute_magnitude_h":21.34,"estimated_diameter":{"kilometers":{"estimated_diameter_min":0.1434019235,"estimated_diameter_max":0.320656449},"meters":{"estimated_diameter_min":143.4019234645,"estimated_diameter_max":320.6564489709},"miles":{"estimated_diameter_min":0.0891057966,"estimated_diameter_max":0.1992466184},"feet":{"estimated_diameter_min":470.4787665793,"estimated_diameter_max":1052.0225040417}},"is_potentially_hazardous_asteroid":false,"close_approach_data":[{"close_approach_date":"2015-09-08","close_approach_date_full":"2015-Sep-08 14:31","epoch_date_close_approach":1441722660000,"relative_velocity":{"kilometers_per_second":"19.7498128142","kilometers_per_hour":"71099.3261312856","miles_per_hour":"44178.3562841869"},"miss_distance":{"astronomical":"0.2591250701","lunar":"100.7996522689","kilometers":"38764558.550560687","miles":"24087179.7459520006"},"orbiting_body":"Earth"}]
