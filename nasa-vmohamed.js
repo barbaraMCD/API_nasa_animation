d53b5e4da9319b7a79546c1623cabdea7df2b436
BOUBTANE <mohamed-ali2000@hotmail.fr>
2022-01-05 17:23:25 +0100

    finallyjs

--- nasa-vmohamed.js
+++ nasa-vmohamed.js
@@ -1,5 +1,5 @@
 const getData = async() => {
-
+    const body = Document
     const dateStart = document.getElementById("start")
     const dateFin = document.getElementById("end")
     const key = "h1l0oYA0cbNuoknqZgeM6JrGnXe2MYnuBa0OFPCG"
@@ -11,7 +11,19 @@ const getData = async() => {
 
     console.log(data);
 
-    resultat.innerHTML = `
-    `
+    const mapsElem = document.createElement("a");
+    mapsElem.className = "mapsButton";
+    mapsElem.innerText = "Google Maps"
+    mapsElem.href = infos.maps.googleMaps;
+
+
+    resultats.innerHTML = `
+  <h3>${pays}</h3>
+  <p><strong>Capital:</strong> ${infos.capital[0]}</p>
+  <p><strong>Continent:</strong> ${infos.continents[0]}</p>
+  <p><strong>Superficie:</strong> ${infos.area}</p>
+  <p><strong>Vents:</strong> ${meteo.wind.speed} km/h</p>
+`
+    console.log(meteo.weather)
+    resultats.appendChild(mapsElem)
 }
-getData()
